6c0d169bb38548a9db96f9f9c16233b2bced910c
Vlad Barboni <vlad.barboni@gmail.com>
2015-09-06 13:30:01 +0200

    Remove "Lange Nacht der Startups" references and add FAQ.

--- src/js/main.js
+++ src/js/main.js
@@ -42,20 +42,3 @@
     });
     closeBttn.addEventListener( 'click', toggleOverlay );
 })();
-
-function initialize() {
-    var mapCanvas = document.getElementById('map');
-    var mapOptions = {
-        center: new google.maps.LatLng(52.515098, 13.395570),
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(52.515098, 13.395570),
-        map: map,
-        icon: 'img/happy-pin-icon.png'
-    });
-}
-google.maps.event.addDomListener(window, 'load', initialize);
